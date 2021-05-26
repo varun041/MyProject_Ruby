0b1df13265ad8a8a1c40701b0578ddf8a16bb4e3
varun.oberoi13@gmail.com <varun.oberoi13@gmail.com>
2021-05-26 14:33:14 +0530

    merged changes

--- myruby.rb
+++ myruby.rb
@@ -1,4 +1,4 @@
-puts "varun oberoi"
-puts "deepa oberoi"
-puts "ashok oberoi"
-puts "geeta oberoi"
+def sum_eq_n?(arr, n)
+    return true if arr.empty? && n == 0
+    arr.product(arr).reject { |a,b| a == b }.any? { |a,b| a + b == n }
+  end
